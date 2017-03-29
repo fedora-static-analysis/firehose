db287f3ab3ad37c1d66301069b5a44b12a33d528
David Malcolm <dmalcolm@redhat.com>
2017-03-29 14:11:11 -0400

    0.4

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@
 from distutils.core import setup
 
 setup(name='firehose',
-    version='0.3',
+    version='0.4',
     description='Library for working with output from static code analyzers',
     packages=['firehose',
               'firehose.parsers'],
