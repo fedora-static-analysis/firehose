b7b950508f8f13aefc231d465f87695494cc235a
Athos Ribeiro <athoscr@fedoraproject.org>
2023-07-27 10:21:47 -0300

    Replace deprecated distutils with setuptools (#44)
    As described in [1], distutils is a deprecated module and will be
    removed in python 3.12. The simplest step forward is substituting it
    with setuptools.
    
    [1] https://docs.python.org/3/library/distutils.html

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 #   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301
 #   USA
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='firehose',
     version='0.5',
